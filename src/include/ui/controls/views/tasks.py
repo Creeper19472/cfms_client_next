8d73c79c30f302f6d98d73265dd619ed4490f248
Creeper19472 <creeper19472@crpteam.club>
2026-01-10 13:29:57 +0800

    fix: revert

--- src/include/ui/controls/views/tasks.py
+++ src/include/ui/controls/views/tasks.py
@@ -148,7 +148,7 @@ class TaskTile(ft.Card):
                 weight=ft.FontWeight.BOLD,
                 color=ft.Colors.WHITE,
             ),
-            bgcolor=ft.Colors.RED,
+            bgcolor=ft.Colors.GREY,
             padding=ft.Padding.symmetric(horizontal=6, vertical=2),
             border_radius=10,
             visible=task.status == DownloadTaskStatus.COMPLETED
